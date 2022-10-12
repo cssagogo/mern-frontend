2bb32198a2fb89c79ed908085d59612784d3aa71
Adam Youngers <adam.youngers@ispot.tv>
2022-10-11 17:15:57 -0700

    MERN stack course - react setup

---
+++ src/users/pages/Users.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import UsersList from '../components/UsersList';
+
+const Users = () => {
+    const USERS = [
+        {
+            id: "1",
+            name: "Adam Youngers",
+            image: "https://pbs.twimg.com/profile_images/1275488441/profile-pic_400x400.jpg",
+            places: 5
+        }
+    ];
+
+    return <UsersList items={USERS} />
+}
+
+export default Users;
