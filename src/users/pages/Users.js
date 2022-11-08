a1d002f9e1d94fa3abe98b764f60f309e28fa697
Adam Youngers <adam.youngers@ispot.tv>
2022-11-07 16:37:31 -0800

    #151 - Hooked users list up to API.

--- src/users/pages/Users.js
+++ src/users/pages/Users.js
@@ -1,18 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 
-import UsersList from '../components/UsersList';
+import UsersList from "../components/UsersList";
+import ErrorModal from "../../shared/components/UIElements/ErrorModal";
+import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
 
 const Users = () => {
-    const USERS = [
-        {
-            id: "u1",
-            name: "Adam Youngers",
-            image: "https://pbs.twimg.com/profile_images/1275488441/profile-pic_400x400.jpg",
-            places: 5
-        }
-    ];
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
+  const [loadedUsers, setLoadedUsers] = useState();
 
-    return <UsersList items={USERS} />
-}
+  useEffect(() => {
+    // TODO: Replace with axios.
+    const sendRequest = async () => {
+      setIsLoading(true);
+      try {
+        const response = await fetch("http://localhost:5000/api/users");
+        const responseData = await response.json();
+        if (!response.ok) {
+          throw new Error(responseData.message);
+        }
+        setLoadedUsers(responseData.users);
+      } catch (err) {
+        setError(err.message);
+      }
+      setIsLoading(false);
+    };
+    sendRequest();
+  }, []);
+
+  const errorHandler = () => {
+    setError(null);
+  };
+
+  return (
+    <>
+      <ErrorModal error={error} onClear={errorHandler} />
+      {isLoading && (
+        <div className="center">
+          <LoadingSpinner />
+        </div>
+      )}
+      {!isLoading && loadedUsers && <UsersList items={loadedUsers} />}
+    </>
+  );
+};
 
 export default Users;
